b2483887ac8c74beff71fd35c6eac99280bb92ef
oliverlai55 <oliverlai55@gmail.com>
2015-11-10 10:36:27 -0500

    Adding scope
    Adding scope with name and desc, list of arrays

---
+++ controller.js
@@ -0,0 +1,26 @@
+angular.module('myApp',[]).controller('myController',function($scope){
+	$scope.items =[
+
+	{
+		name: 'Freddy',
+		desc: 'Made a cool football game'
+	},
+	{
+		name: 'Chance',
+		desc: 'Bootstrap Guy'
+	},
+	{
+		name: 'Will',
+		desc: 'The wig guy'
+	},
+	{
+		name: 'Stephen',
+		desc: 'Awesome theme'
+	},
+	{
+		name: 'Griffin',
+		desc: 'Eats 8 times/day'
+	},
+
+	];
+});
